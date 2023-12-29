f0d0e926847699649e231fab559ec80b88aeccb3
Weiss Bai <whitesgr03@gmail.com>
2023-12-29 17:29:31 +0800

    Add readFile page404

--- index.js
+++ index.js
@@ -4,32 +4,28 @@ const fs = require("node:fs");
 
 const PORT = process.env.PORT || 8080;
 
+let page404 = null;
+
 const getPage = async url => {
 	let path =
 		url === "/" ? `${__dirname}/index.html` : `${__dirname + url}.html`;
 
-	const hasPath = fs.existsSync(path);
-
-	!hasPath && (path = `${__dirname}/404.html`);
-
 	try {
-		return {
-			page: await fsPromises.readFile(path),
-			hasPath,
-		};
+		return await fsPromises.readFile(path);
 	} catch (err) {
-		console.err(err);
-		return { page: null };
+		console.error(err);
+		return false;
 	}
 };
 
 const server = http.createServer(async (req, res) => {
-	const { page, hasPath } = await getPage(req.url);
+	const page = await getPage(req.url);
 
-	res.writeHead(hasPath ? 200 : 404, { "Content-Type": "text/html" });
-	res.end(page ? page : "<h1>404 Not Found</h1>");
+	res.writeHead(page ? 200 : 404, { "Content-Type": "text/html" });
+	res.end(page ? page : page404);
 });
 
-server.listen(PORT, () => {
+server.listen(PORT, async () => {
+	page404 = await fsPromises.readFile(`${__dirname}/404.html`);
 	console.log(`Server is running at http://localhost:${PORT}`);
 });
